246eea7db45403b8f385631eb59d093f481fcd5f
Lenovo <Lenovo@DESKTOP-GCO8406>
2021-12-23 23:52:50 +0530

    Added Unbounded Binary Search application , Searching in an infinite array

--- TestProject/src/searching/Input.java
+++ TestProject/src/searching/Input.java
@@ -34,9 +34,16 @@ public class Input {
 //		int l = ls.getLastOccurence(arr, 9);
 //		System.out.println(l - f);
 		
-		int x = 23;
-		Root r = new Root();
-		System.out.println(r.findSqrt(x));
+//		int x = 23;
+//		Root r = new Root();
+//		System.out.println(r.findSqrt(x));
+		
+		long[] arr = new long[1000000];
+		for(int i = 0; i < arr.length;i++) {
+			arr[i] = i-10;
+		}
+		InfiniteSizedArraySearch is = new InfiniteSizedArraySearch();
+		System.out.println(is.search(arr, 0));
 		
 	}
 
