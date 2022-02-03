6974b0988d07e34ed05da4a23470b1e84d9e6eb4
nigam <nigam@localhost>
2022-02-03 06:04:37 +0530

    Search in Sorted Row and column Matrix added

--- TestProject/src/matrixOR2Darrays/input.java
+++ TestProject/src/matrixOR2Darrays/input.java
@@ -17,10 +17,16 @@ public class input {
 //		TransposeAndPrint trp = new TransposeAndPrint();
 //		trp.transpose(array);
 		
+//		int array[][] = {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}};
+//		PrintSpiralMatrrix prs = new PrintSpiralMatrrix();
+//		int arr[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+//		prs.print(array, array.length, array[0].length);
+//		prs.print(arr, 3, 4);
+		
+		
 		int array[][] = {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}};
-		PrintSpiralMatrrix prs = new PrintSpiralMatrrix();
-		int arr[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
-		prs.print(array, array.length, array[0].length);
-		prs.print(arr, 3, 4);
+		SearchInASortedRowAndColumnMatrix srp = new SearchInASortedRowAndColumnMatrix();
+		int ans[] = srp.search(array, 4,4 , 55);
+		System.out.println(ans[0]+", "+ans[1]);
 	}
 }
