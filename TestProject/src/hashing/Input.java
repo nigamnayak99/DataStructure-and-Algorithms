265521abe4f3f28ed132a219a1dd2c3b85067e60
nigam <nigam@localhost>
2022-02-14 08:19:31 +0530

    Open Chaining added

--- TestProject/src/hashing/Input.java
+++ TestProject/src/hashing/Input.java
@@ -4,15 +4,15 @@ public class Input {
 
 	public static void main(String[] args) {
 		
-		MyHashChaining msc = new MyHashChaining(7);
-		msc.insert(70);
-		msc.insert(71);
-		msc.insert(9);
-		msc.insert(56);
-		msc.insert(72);
-		
-		System.out.println(msc.search(20));
-		System.out.println(msc.search(72));
+//		MyHashChaining msc = new MyHashChaining(7);
+//		msc.insert(70);
+//		msc.insert(71);
+//		msc.insert(9);
+//		msc.insert(56);
+//		msc.insert(72);
+//		
+//		System.out.println(msc.search(20));
+//		System.out.println(msc.search(72));
 
 	}
 
